e0d6a751e11de7cef429e8aade57a8d07bae9e3f
Aldjia Boughias <boughias-aldjia@live.fr>
2025-06-21 18:30:11 +0200

    add redirect from / to /plant

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,24 @@ Rails.application.routes.draw do
   get "care_logs/index"
   get "achievements/index"
   get "profiles/show"
+
   devise_for :users
+
+  # redirect root to the plant page
+  root to: redirect("/plant")
+
   get  "/plant",        to: "plants#show",      as: :plant
   post "/plant/water",  to: "plants#water",     as: :water_plant
   post "/plant/quest",  to: "plants#do_quest",  as: :quest_plant
   post "/plant/quest/answer", to: "plants#submit_quest_response", as: :submit_quest_response
 
-  resource :plant, only: [:show, :new, :create, :edit, :update]
-  resource :profile, only: [:show]
-  get 'mon_profil', to: 'users#profile', as: 'mon_profil'
+  resource :plant, only: [ :show, :new, :create, :edit, :update ]
+  resource :profile, only: [ :show ]
+
+  get "mon_profil", to: "users#profile", as: "mon_profil"
   get "achievements", to: "achievements#index", as: :achievements
   get "/historique", to: "care_logs#index", as: :care_logs
-  resources :care_logs, only: [:index]
+  resources :care_logs, only: [ :index ]
 
   get "quiz/start", to: "quiz#start", as: :quiz_start
   get "quiz/show", to: "quiz#show", as: :quiz_show
